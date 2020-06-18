7c46d5a8eef78925276e555769c6c870e2cf90db
matt-r-mck <matt.r.mck@gmail.com>
2020-06-18 15:35:31 -0400

    Add project files.

---
+++ StudentClassPractice/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq.Expressions;
+using StudentClassPractice;
+
+namespace StudentClassPractice {
+    class Program {
+        static void Main(string[] args) {
+
+            var jada = new Student(); //
+            jada.Name = "Jada";
+            jada.ID = 1;
+            jada.GPA = 3.5;
+            jada.SAT = 1600;
+            jada.TimesTardy = 1112;
+            Console.WriteLine($"Jada's name is {jada.Name}");
+
+            var steve = new Student();
+            steve.Name = "Steve";
+            steve.ID = 2;
+            steve.GPA = 4.0;
+            steve.SAT = 1599;
+            steve.TimesTardy = 0;
+            var SteveString = steve.GetValues();
+            Console.WriteLine($"Steve: {SteveString}");
+            //Calls the BC method for steve's instance
+            Console.WriteLine($"Steve's BC score is {steve.BC(2)}");
+
+            var dave = new Student(3.3, 1150, 5, "Dave", 3);
+            var DaveString = dave.GetValues();
+            Console.WriteLine($"{DaveString}");
+        }
+    }
+}   
---
+++ StudentClassPractice/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace StudentClassPractice {
+
+     class Student {
+        public double GPA;
+        public int SAT;
+        public int TimesTardy;
+        public string Name;
+        public int ID;
+
+        private double SecretNumberCalculation(double Bnumber, int SecretNumber) {
+            return Bnumber / SecretNumber;
+        }//calculates BC number secretly
+
+        public double BC(int SecretNumber) {
+            var dblSAT = SAT * 2;
+            var b = dblSAT * GPA;
+            var bc = SecretNumberCalculation (b, SecretNumber);
+            return bc;
+        }//calculates BC number publically
+
+        public string GetValues() {
+            return $"{ID}|{Name}|{SAT}|{GPA}|{TimesTardy}";
+            //shows what exists in instance
+
+            }
+        public Student() {//default constructor
+            this.ID = 0;
+            this.GPA = 2.0;
+            this.SAT = 1000;
+        }
+
+        public Student(double GPA, int SAT, int TimesTardy, string Name, int ID) {
+            this.GPA = GPA;
+            this.SAT = SAT;
+            this.TimesTardy = TimesTardy;
+            this.Name = Name; //when parameter matches variable, must call this._
+            this.ID = ID;
+
+        }
+    } 
+}
